1a7dbc15f4479db6053f3f74f4eee02623a50b74
Manish <koiralamanish286@gmail.com>
2026-02-10 09:34:30 +0545

    adding a palindrome check

---
+++ Day1/palindrome.py
@@ -0,0 +1,9 @@
+def is_palindrome(s):
+    return s == s[::-1]
+
+if __name__ == "__main__":
+    word = "madam"
+    if is_palindrome(word):
+        print(f"{word} is a palindrome")
+    else:
+        print(f"{word} is not a palindrome")
