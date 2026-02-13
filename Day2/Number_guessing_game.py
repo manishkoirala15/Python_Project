17212f21f68f8e6e4bd1e6d9f86ff415c0f41220
Manish Koirala <koiralamanish286@gmail.com>
2026-02-13 18:57:18 +0545

    changed number guessing game

--- Day2/Number_guessing_game.py
+++ Day2/Number_guessing_game.py
@@ -6,7 +6,7 @@ attempts = 0
 print("Guess the number between 1 and 20")
 
 while True:
-    guess = int(input("Enter your guess: "))
+    guess = int (input("Enter your guess: "))
     attempts += 1
     
     if guess < number_to_guess:
